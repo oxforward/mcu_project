6fa18fbc94bcdba22ce0d9f3ee036f4ebbf26f60
oxforward <oxforward@163.com>
2015-05-26 22:12:48 +0800

    general-backup

--- adc/user/adc.c
+++ adc/user/adc.c
@@ -1,2 +1,50 @@
 #include<reg52.h>
-#include<intrins.h>
+
+sbit start = P3^0;
+sbit eoc = P3^1;
+sbit oe = P3^2;
+sbit ale = P3^3;
+sbit clk = P3^4;
+sbit lock = P3^5;
+void chose_channel();
+void delay();
+void main()
+{
+	TMOD=0x02;      //定义T0,工作方式2
+	TH0=0x216;      //TH0初值
+	TL0=0x216;   //TH1初值
+	EA=1;     //打开总中断
+	ET0=1;    //打开TO中断允许位
+	TR0=1;     //打开T0
+	while(1){
+		chose_channel();
+		while(!eoc);
+		lock = 1;
+		oe = 1;
+		delay();
+		lock = 0;
+		oe = 0;			
+	}
+}
+
+void inter_1() interrupt 1
+{
+ clk = ~clk;
+}
+
+
+void delay()
+{
+	int i,j;
+	for(i = 1000; i < 0; i--)
+		for(j = 1000; j < 0; j--);
+}
+
+void chose_channel()
+{
+	ale = 0;
+	
+	start = 1;
+	delay();
+	start = 0;	
+}
--- exam4/user/exam4.c
+++ exam4/user/exam4.c
@@ -1,40 +1,28 @@
 /******************************
-*
-*60S倒计时实验
-*时间与实际时间有点出入
+*exam_name:	display 59s time on the digital tube through the P1 port
 ******************************/
 #include<reg52.h>
-#define uchar unsigned char
 #define uint unsigned int
-
+#define uchar unsigned char
 sbit bit1 = P2^0;
 sbit bit2 = P2^1;
 int countTime = 59;
 int ten = 5,unit = 9, count = 0;
 
-uchar code num[]={			//Common anode LED code table
+uchar code num[] = {			//Common anode LED code table
 0xc0, 0xf9, 0xa4, 0xb0,
 0x99, 0x92, 0x82, 0xf8,
 0x80, 0x90, 0x88, 0x83,
 0xc6, 0xa1, 0x86, 0x8e};
 
-void delay_on(uint delay_time)		//Delay function
-{
-	int i;
-	int j;
-	for(i=0; i<=delay_time; i++)
-		for(j=0; j<=delay_time; j++);
-}
+void init();
+void delay_on(uint delay_time);
 
-void init()
-{
-	TH0 = (65536 - 50000) / 256;
-	TL0 = (65536 - 50000) % 256;
-	TMOD = 0x01;
-	ET0 = 1;
-	EA = 1;
-}
-
+/****************************************
+*function:	display the 59s time on the digital tube through the P1 port
+*parameter:	none
+*return:	none
+****************************************/
 void main()
 {
 	init();
@@ -55,6 +43,32 @@ void main()
 	}
 }
 
+/**********************************
+*function:	initilize the Timer 0
+*Parameter:	none
+*return:	none
+**********************************/
+void init()
+{
+	TH0 = (65536 - 50000) / 256;			//Initial value
+	TL0 = (65536 - 50000) % 256;
+	TMOD = 0x01;
+	ET0 = 1;
+	EA = 1;
+}
+
+void delay_on(uint delay_time)		//Delay function
+{
+	int i;
+	int j;
+	for(i=0; i<=delay_time; i++)
+		for(j=0; j<=delay_time; j++);
+}
+
+
+/********************************************
+*function:	timer 0 interrupt function for count time 
+*********************************************/
 void timer_0() interrupt 1
 {
 	TH0 = (65536 - 50000) / 256;
@@ -62,8 +76,8 @@ void timer_0() interrupt 1
 	count++;
 	if(count==20){
 		count = 0;
-		countTime--;
-		if(countTime == -1){
+		countTime--;			//countTime is a global value in order to ensure the current number
+		if(countTime < 0){
 			countTime = 59;			
 		}
 		ten = countTime / 10;
